b4ea41c86a736ce5d3714a2dad51b67996408fbf
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-03-06 15:18:34 +0100

    Lesson 69: The Beauty of Create React App's Proxy

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Hi there! Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href={"/auth/google"}>Sign in with Google</a>
         <a
           className="App-link"
           href="https://reactjs.org"
