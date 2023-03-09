fe1bc5b09171b23621be4608226cf1ed1f01b9a2
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-03-09 12:52:55 +0100

    Lesson 76: Installing Root Modules

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render();
+import App from './components/App';
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
