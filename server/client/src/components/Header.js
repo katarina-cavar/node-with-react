1b2ed490a04699331ca683a487b559213b46d754
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-06-06 13:54:41 +0200

    Lesson 89: Header Design

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>
-                Header
-            </div>
+            <nav>
+                <div className={"nav-wrapper"}>
+                    <a className={"left brand-logo"}>
+                        Emaily
+                    </a>
+                    <ul className={"right"}>
+                        <a>Login with Google</a>
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
