5ead013c0a32e86d4af12e61742a56caa731dab0
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-06-07 11:39:49 +0200

    Lesson 94: Testing FetchUser

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,18 +1,11 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
+export const fetchUser = () => {
     return function(dispatch) {
         axios.get('/api/current_user')
-            .then(res => dispatch({ type: FETCH_USER, payload: res }));
-    }
-}
-
-// const fetchUser = () => {
-//     const response = axios.get('api/current_user');
-//
-//     return  {
-//         type: FETCH_USER,
-//         payload: response
-//     };
-// }
+            .then(res => dispatch(
+                { type: FETCH_USER, payload: res })
+            );
+    };
+};
--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
@@ -7,7 +10,7 @@ const Landing = () => <h2>Landing</h2>;
 
 class App extends Component {
     componentDidMount() {
-
+        this.props.fetchUser();
     }
 
     render() {
@@ -26,4 +29,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(null, actions)(App);
