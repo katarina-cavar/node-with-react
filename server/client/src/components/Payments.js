84c1d17c5a6302d231d1e71f108c2b7dc0f095c6
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-07-01 22:57:12 +0200

    Lesson 111: Payment Fixes

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -5,10 +5,16 @@ class Payments extends Component {
     render () {
         return (
             <StripeCheckout
+                name={'Emaily'}
+                description={'$5 for 5 email credits'}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
                 amount={500}
-            />
+            >
+                <button className={'btn'}>
+                    Add Credits
+                </button>
+            </StripeCheckout>
         );
     }
 }
