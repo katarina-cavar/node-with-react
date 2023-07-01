3f7326f93a3c71c581f8358141a5133448b3f337
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-07-01 22:41:41 +0200

    Lesson 109: The Payments Component

---
+++ server/client/src/components/Payments.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+class Payments extends Component {
+    render () {
+        debugger;
+
+        return (
+            <StripeCheckout
+                token={token => console.log(token)}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+                amount={500}
+            />
+        );
+    }
+}
+
+export default Payments;
