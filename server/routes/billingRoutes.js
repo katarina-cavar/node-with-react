a398bf2eacf3d6f3986348b89310e23abe0faff0
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-07-05 14:34:18 +0200

    Lesson 120: Requiring Authentication

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -4,7 +4,10 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
     app.post('/api/stripe', async (req, res) => {
-        console.log(req.body);
+        if (!req.user) {
+            return res.status(401).send({ error: 'You must log in!' });
+        }
+
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
