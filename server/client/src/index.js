01708867b2e100a3898ebfd2273f54c48f89a359
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-08-04 12:49:06 +0200

    Lesson 154: Improving the Email Template

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -8,6 +8,10 @@ import App from './components/App';
 import reducers from './reducers';
 import reduxThunk from 'redux-thunk';
 
+// TODO: remove - development only axios helpers
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
