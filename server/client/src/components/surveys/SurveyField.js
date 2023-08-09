18572c0945d1b9c36fd42e0b0b16bba526fa542d
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-08-09 13:17:32 +0200

    Lesson 172: Wiring up Custom Fields

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -1,11 +1,13 @@
 // SurveyField contains logic to render a single label and text input
 import React, { Component } from 'react';
 
-export default () => {
+export default ({ input }) => {
+    console.log(input)
+
     return (
         <div>
             SurveyField
-            <input />
+            <input {...input} />
         </div>
     );
 };
