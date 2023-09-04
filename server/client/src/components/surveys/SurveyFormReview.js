cab0c6468c57846bbd1cac8a0669fafd6db1af91
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-09-04 11:45:06 +0200

    Lesson 184: Persisting Form Values

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,18 +1,20 @@
 // SurveyFormReview shows users their form inputs for review
 import React from 'react';
+import { connect } from 'react-redux';
 
 const SurveyReview = ({ onCancel }) => {
     return (
         <div>
             <h5>Please confirm your entries</h5>
-            <button
-                className={'yellow darken-3 btn-flat'}
-                onClick={onCancel}
-            >
+            <button className={'yellow darken-3 btn-flat'} onClick={onCancel}>
                 Back
             </button>
         </div>
     );
 };
 
-export default SurveyReview;
+function mapStateToProps(state) {
+    return { formValues: state.form.surveyForm.values };
+}
+
+export default connect(mapStateToProps)(SurveyReview);
