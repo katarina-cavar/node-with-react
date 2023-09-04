b1526dc963e09ccde5eb80e7a3ede81591a69909
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2023-09-04 11:55:21 +0200

    Lesson 185: Refactoring Form Fields - mid session

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -2,10 +2,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SurveyReview = ({ onCancel }) => {
+const SurveyReview = ({ onCancel, formValues }) => {
     return (
         <div>
             <h5>Please confirm your entries</h5>
+            <div>
+                <div>
+                    <label>Survey Title</label>
+                    <div>{formValues.title}</div>
+                </div>
+                <div>
+                    <label>Subject Line</label>
+                    <div>{formValues.subject}</div>
+                </div>
+                <div>
+                    <label>Email Body</label>
+                    <div>{formValues.body}</div>
+                </div>
+                <div>
+                    <label>Recipient List</label>
+                    <div>{formValues.emails}</div>
+                </div>
+            </div>
+
             <button className={'yellow darken-3 btn-flat'} onClick={onCancel}>
                 Back
             </button>
